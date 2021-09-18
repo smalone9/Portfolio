1aa658a4158d7d4762d784417f942370d713dedc
Samantha Malone <sammac99@gmail.com>
2021-09-17 18:01:46 -0700

    added resume component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,108 +1,237 @@
 import React from "react";
-import { makeStyles, withStyles } from "@material-ui/core/styles";
-import Box from "@material-ui/core/Box";
-import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
-
-import Send from "@material-ui/icons/Send";
+import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles((theme) => ({
-  contactContainer: {
+  mainContainer: {
     background: "#233",
-    height: "100vh",
+  },
+  timeLine: {
+    position: "relative",
+    padding: "1rem",
+    margin: "o auto",
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      height: "100%",
+      border: "1px solid tan",
+      right: "40px",
+      top: 0,
+    },
+    "&:after": {
+      content: "''",
+      display: "table",
+      clear: "both",
+    },
+    [theme.breakpoints.up("md")]: {
+      padding: "2rem",
+      "&:before": {
+        left: "calc(50% - 1px)",
+        right: "auto",
+      },
+    },
+  },
+  timeLineItem: {
+    padding: "1rem",
+    borderBottom: "2px solid tan",
+    position: "relative",
+    margin: "1rem 3rem 1rem 1rem",
+    clear: "both",
+    "&:after": {
+      content: "''",
+      position: "absolute",
+    },
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      right: "-0.625rem",
+      top: "calc(50% - 5px)",
+      borderStyle: "solid",
+      borderColor: "tomato tomato transparent transparent",
+      borderWidth: "0.625rem",
+      transform: "rotate(45deg)",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "44%",
+      margin: "1rem",
+      "&:nth-of-type(2n)": {
+        float: "right",
+        margin: "1rem",
+        borderColor: "tan",
+      },
+      "&:nth-of-type(2n):before": {
+        right: "auto",
+        left: "-0.625rem",
+        borderColor: "transparent transparent tomato tomato",
+      },
+    },
+  },
+  timeLineYear: {
+    textAlign: "center",
+    maxWidth: "9.375rem",
+    margin: "0 3rem 0 auto",
+    fontSize: "1.8rem",
+    color: "#fff",
+    background: "tomato",
+    lineHeight: 1,
+    padding: "0.5rem 1rem",
+    "&:before": {
+      display: "none",
+    },
+    [theme.breakpoints.up("md")]: {
+      textAlign: "center",
+      margin: "0 auto",
+      "&:nth-of-type(2n)": {
+        float: "none",
+        margin: "0 auto",
+      },
+      "&:nth-of-type(2n):before": {
+        display: "none",
+      },
+    },
   },
   heading: {
     color: "tomato",
-    textAlign: "center",
+    padding: "3rem 0",
     textTransform: "uppercase",
-    marginBottom: "1rem",
   },
-  form: {
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    position: "absolute",
-  },
-  input: {
+  subHeading: {
     color: "#fff",
+    padding: 0,
+    textTransform: "uppercase",
   },
-  button: {
-    marginTop: "1rem",
+  body1: {
     color: "tomato",
-    borderColor: "tan",
   },
-  field: {
-    margin: "1rem 0rem",
+  subtitle1: {
+    color: "tan",
   },
 }));
 
-const InputField = withStyles({
-  root: {
-    "& label.Mui-focused": {
-      color: "tomato",
-    },
-    "& label": {
-      color: "tan",
-    },
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        borderColor: "tan",
-      },
-      "&:hover fieldset": {
-        borderColor: "tan",
-      },
-      "&.Mui-focused fieldset": {
-        color: "#fff",
-        borderColor: "tan",
-      },
-    },
-  },
-})(TextField);
-
-const Contact = () => {
+const Resume = () => {
   const classes = useStyles();
   return (
-    <Box component="div" className={classes.contactContainer}>
-      <Grid container justify="center">
-        <Box component="form" className={classes.form}>
-          <Typography variant="h5" className={classes.heading}>
-            Hire or Contact me...
-          </Typography>
-          <InputField
-            fullWidth={true}
-            label="Name"
-            variant="outlined"
-            inputProps={{ className: classes.input }}
-          />
-          <InputField
-            fullWidth={true}
-            label="Email"
-            variant="outlined"
-            inputProps={{ className: classes.input }}
-            className={classes.field}
-          />
-          <InputField
-            fullWidth={true}
-            label="Message"
-            variant="outlined"
-            multiline
-            rows={4}
-            inputProps={{ className: classes.input }}
-          />
-          <Button
-            variant="outlined"
-            fullWidth={true}
-            endIcon={<Send />}
-            className={classes.button}
+    <Box component="header" className={classes.mainContainer}>
+      <Typography variant="h4" align="center" className={classes.heading}>
+        Working Experience
+      </Typography>
+      <Box component="div" className={classes.timeLine}>
+        <Typography
+          variant="h2"
+          className={`${classes.timeLineYear} ${classes.timeLineItem}`}
+        >
+          2013
+        </Typography>
+        <Box component="div" className={classes.timeLineItem}>
+          <Typography
+            variant="h5"
+            align="center"
+            className={classes.subHeading}
           >
-            Contact Me
-          </Button>
+            web design
+          </Typography>
+          <Typography variant="body1" align="center" className={classes.body1}>
+            company name where worked
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            className={classes.subtitle1}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quas
+            ipsa, laudantium totam perferendis possimus voluptatibus tenetur.
+            Quasi voluptatibus, nam vitae eaque ad, officia laboriosam
+            repudiandae, rerum necessitatibus nisi mollitia.
+          </Typography>
         </Box>
-      </Grid>
+        <Typography
+          variant="h2"
+          className={`${classes.timeLineYear} ${classes.timeLineItem}`}
+        >
+          2014
+        </Typography>
+        <Box component="div" className={classes.timeLineItem}>
+          <Typography
+            variant="h5"
+            align="center"
+            className={classes.subHeading}
+          >
+            html & css
+          </Typography>
+          <Typography variant="body1" align="center" className={classes.body1}>
+            company name where worked
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            className={classes.subtitle1}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quas
+            ipsa, laudantium totam perferendis possimus voluptatibus tenetur.
+            Quasi voluptatibus, nam vitae eaque ad, officia laboriosam
+            repudiandae, rerum necessitatibus nisi mollitia.
+          </Typography>
+        </Box>
+        <Typography
+          variant="h2"
+          className={`${classes.timeLineYear} ${classes.timeLineItem}`}
+        >
+          2015
+        </Typography>
+        <Box component="div" className={classes.timeLineItem}>
+          <Typography
+            variant="h5"
+            align="center"
+            className={classes.subHeading}
+          >
+            Fullstack Javascript
+          </Typography>
+          <Typography variant="body1" align="center" className={classes.body1}>
+            company name where worked
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            className={classes.subtitle1}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quas
+            ipsa, laudantium totam perferendis possimus voluptatibus tenetur.
+            Quasi voluptatibus, nam vitae eaque ad, officia laboriosam
+            repudiandae, rerum necessitatibus nisi mollitia.
+          </Typography>
+        </Box>
+        <Typography
+          variant="h2"
+          className={`${classes.timeLineYear} ${classes.timeLineItem}`}
+        >
+          2017
+        </Typography>
+        <Box component="div" className={classes.timeLineItem}>
+          <Typography
+            variant="h5"
+            align="center"
+            className={classes.subHeading}
+          >
+            Django & React
+          </Typography>
+          <Typography variant="body1" align="center" className={classes.body1}>
+            company name where worked
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            className={classes.subtitle1}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quas
+            ipsa, laudantium totam perferendis possimus voluptatibus tenetur.
+            Quasi voluptatibus, nam vitae eaque ad, officia laboriosam
+            repudiandae, rerum necessitatibus nisi mollitia.
+          </Typography>
+        </Box>
+      </Box>
     </Box>
   );
 };
 
-export default Contact;
+export default Resume;
