44925e648026a5a2d81e71db6b15338535b8e6ba
Samantha Malone <sammac99@gmail.com>
2021-09-18 12:15:14 -0700

    styled resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,112 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 
-const useStyles = makeStyles((theme) => ({
-  mainContainer: {
-    background: "#233",
-  },
-  timeLine: {
-    position: "relative",
-    padding: "1rem",
-    margin: "o auto",
-    "&:before": {
-      content: "''",
-      position: "absolute",
-      height: "100%",
-      border: "1px solid tan",
-      right: "40px",
-      top: 0,
-    },
-    "&:after": {
-      content: "''",
-      display: "table",
-      clear: "both",
-    },
-    [theme.breakpoints.up("md")]: {
-      padding: "2rem",
-      "&:before": {
-        left: "calc(50% - 1px)",
-        right: "auto",
-      },
-    },
-  },
-  timeLineItem: {
-    padding: "1rem",
-    borderBottom: "2px solid tan",
-    position: "relative",
-    margin: "1rem 3rem 1rem 1rem",
-    clear: "both",
-    "&:after": {
-      content: "''",
-      position: "absolute",
-    },
-    "&:before": {
-      content: "''",
-      position: "absolute",
-      right: "-0.625rem",
-      top: "calc(50% - 5px)",
-      borderStyle: "solid",
-      borderColor: "tomato tomato transparent transparent",
-      borderWidth: "0.625rem",
-      transform: "rotate(45deg)",
-    },
-    [theme.breakpoints.up("md")]: {
-      width: "44%",
-      margin: "1rem",
-      "&:nth-of-type(2n)": {
-        float: "right",
-        margin: "1rem",
-        borderColor: "tan",
-      },
-      "&:nth-of-type(2n):before": {
-        right: "auto",
-        left: "-0.625rem",
-        borderColor: "transparent transparent tomato tomato",
-      },
-    },
-  },
-  timeLineYear: {
-    textAlign: "center",
-    maxWidth: "9.375rem",
-    margin: "0 3rem 0 auto",
-    fontSize: "1.8rem",
-    color: "#fff",
-    background: "tomato",
-    lineHeight: 1,
-    padding: "0.5rem 1rem",
-    "&:before": {
-      display: "none",
-    },
-    [theme.breakpoints.up("md")]: {
-      textAlign: "center",
-      margin: "0 auto",
-      "&:nth-of-type(2n)": {
-        float: "none",
-        margin: "0 auto",
-      },
-      "&:nth-of-type(2n):before": {
-        display: "none",
-      },
-    },
-  },
-  heading: {
-    color: "tomato",
-    padding: "3rem 0",
-    textTransform: "uppercase",
-  },
-  subHeading: {
-    color: "#fff",
-    padding: 0,
-    textTransform: "uppercase",
-  },
-  body1: {
-    color: "tomato",
-  },
-  subtitle1: {
-    color: "tan",
-  },
-}));
-
+// resume component function
 const Resume = () => {
   const classes = useStyles();
   return (
@@ -230,3 +125,110 @@ const Resume = () => {
 };
 
 export default Resume;
+
+// css using material ui styles
+const useStyles = makeStyles((theme) => ({
+  mainContainer: {
+    background: "#078",
+  },
+  timeLine: {
+    position: "relative",
+    padding: "1rem",
+    margin: "o auto",
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      height: "100%",
+      border: "1px solid tan",
+      right: "40px",
+      top: 0,
+    },
+    "&:after": {
+      content: "''",
+      display: "table",
+      clear: "both",
+    },
+    [theme.breakpoints.up("md")]: {
+      padding: "2rem",
+      "&:before": {
+        left: "calc(50% - 1px)",
+        right: "auto",
+      },
+    },
+  },
+  timeLineItem: {
+    padding: "1rem",
+    borderBottom: "2px solid tan",
+    position: "relative",
+    margin: "1rem 3rem 1rem 1rem",
+    clear: "both",
+    "&:after": {
+      content: "''",
+      position: "absolute",
+    },
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      right: "-0.625rem",
+      top: "calc(50% - 5px)",
+      borderStyle: "solid",
+      borderColor: "beige beige transparent transparent",
+      borderWidth: "0.625rem",
+      transform: "rotate(45deg)",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "44%",
+      margin: "1rem",
+      "&:nth-of-type(2n)": {
+        float: "right",
+        margin: "1rem",
+        borderColor: "tan",
+      },
+      "&:nth-of-type(2n):before": {
+        right: "auto",
+        left: "-0.625rem",
+        borderColor: "transparent transparent beige beige",
+      },
+    },
+  },
+  timeLineYear: {
+    textAlign: "center",
+    maxWidth: "9.375rem",
+    margin: "0 3rem 0 auto",
+    fontSize: "1.8rem",
+    color: "#fff",
+    background: "darkBlue",
+    lineHeight: 1,
+    padding: "0.5rem 1rem",
+    "&:before": {
+      display: "none",
+    },
+    [theme.breakpoints.up("md")]: {
+      textAlign: "center",
+      margin: "0 auto",
+      "&:nth-of-type(2n)": {
+        float: "none",
+        margin: "0 auto",
+      },
+      "&:nth-of-type(2n):before": {
+        display: "none",
+      },
+    },
+  },
+  heading: {
+    color: "white",
+    padding: "3rem 0",
+    textTransform: "uppercase",
+  },
+  subHeading: {
+    color: "#fff",
+    padding: 0,
+    textTransform: "uppercase",
+  },
+  body1: {
+    color: "darkBlue",
+  },
+  subtitle1: {
+    color: "beige",
+  },
+}));
